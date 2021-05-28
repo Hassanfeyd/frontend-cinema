614ce514820f687451ddbaea21a8df8b91ed6c1f
hassan feyd <hassanfeyd13@gmail.com>
2021-05-28 14:04:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navigation from './nav'
+import Cinema from './component/cinema'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div>
+    
+    <div className="row">
+       <Navigation/>
     </div>
+    <div className="row">
+    <Cinema/>
+    </div>
+  </div>
+  
+   
   );
 }
 
